e09ae5f432f50c777214fc2304ac5814ce275397
weaselflink <stefan@stefanbissell.de>
2021-03-13 22:11:18 +0100

    clean up

--- experiments.py
+++ experiments.py
@@ -1,15 +1,10 @@
 
-import sc2
 from sc2.constants import *
-from sc2 import Union
-from sc2.position import Point2
-from sc2.unit import Unit
+
+from spin_bot import SpinBot
 
 
-class FirstBot(sc2.BotAI):
-
-    def has_building(self, where: Union[Unit, Point2]):
-        return self.structures.closest_distance_to(where) < 0.5
+class FirstBot(SpinBot):
 
     async def build_depots(self):
         if self.supply_left < 2 and self.can_afford(UnitTypeId.SUPPLYDEPOT) and not self.already_pending(UnitTypeId.SUPPLYDEPOT):

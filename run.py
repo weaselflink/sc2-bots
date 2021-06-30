7919e0121616f4143ba5c31ecf44d48e16ae44eb
Stefan Bissell <stefan.bissell@mhp.com>
2021-06-30 11:54:24 +0200

    clean code

--- run.py
+++ run.py
@@ -3,8 +3,18 @@ from sc2.player import Bot, Computer
 
 from spin_bot import SpinBot
 
-run_game(
-    maps.get("EphemeronLE"),
-    [Bot(Race.Terran, SpinBot()), Computer(Race.Protoss, Difficulty.Hard)],
-    realtime=False
-)
+
+def main():
+    player_config = [
+        Bot(Race.Terran, SpinBot()),
+        Computer(Race.Protoss, Difficulty.Hard)
+    ]
+    run_game(
+        maps.get("EphemeronLE"),
+        player_config,
+        realtime=False
+    )
+
+
+if __name__ == "__main__":
+    main()
